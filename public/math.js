f4fea035d90f4cef45380e8da11cb75843c2e86b
Bob <56490747+gamyr@users.noreply.github.com>
2022-02-12 10:42:48 -0800

    Add files via upload

---
+++ public/math.js
@@ -0,0 +1,106 @@
+document.write('\
+\
+    <h1>Add your HTML code here</h1>\
+\
+     <p>Notice however, that you have to escape LF's with a '\', just like\
+        demonstrated in this code listing.\
+    </p>\
+\
+<body><table width="778" border="0" align="center" cellpadding="0" cellspacing="0"><tr><td><img src="../html/images/spacer.gif" alt="" width="1" height="1"></td></tr></table><div align="center"><table width="900" border="0" cellpadding="0" cellspacing="0" height="75"><tr valign="top"><td width="900" height="100%"><table width="1000"  border="0" cellspacing="0" cellpadding="0" height="86"><tr><td background="../images/Top_banner_green_flying_cubes.jpg" width="100%"><img src="../html/images/spacer.gif" alt="" width="1" height="1"></td></tr></table></td></tr></table></div><div align="center"><table width="1000" border="0" cellpadding="0" cellspacing="0"><tr><td height="35" width="160" bgcolor="#051492" style="border-top-style: double; border-top-width: 3px">&nbsp;</td><td height="35" width="170" bgcolor="#051492" align="center" style="border-top-style: double; border-top-width: 3px"><b><font color="#FFFF00" size="2" face="Kristen ITC"><a href="../index.html" style="text-decoration: none"><font color="#FFFF00">Home</font></a></font></b></td><td height="35" width="188" bgcolor="#051492" align="center" style="border-top-style: double; border-top-width: 3px"><b><font face="Kristen ITC" size="2" color="#FFFF00"><a href="../Contact_Us.html" style="text-decoration: none"><font color="#FFFF00">Contact Us</font></a></font></b></td><td height="35" width="193" bgcolor="#051492" align="center" style="border-top-style: double; border-top-width: 3px"><b><font color="#FFFF00" size="2" face="Kristen ITC"><a href="../Parent_Corner.htm" style="text-decoration: none"><font color="#FFFF00">Parents Corner</font></a></font></b></td><td height="35" width="152" bgcolor="#051492" align="center" style="border-top-style: double; border-top-width: 3px"><b><font color="#FFFF00" size="2" face="Kristen ITC"><a href="../about_us.htm" style="text-decoration: none"><font color="#FFFF00">About Us</font></a></font></b></td><td height="35" width="129" bgcolor="#051492" align="center" style="border-top-style: double; border-top-width: 3px"><b><font color="#FFFF00" size="2" face="Kristen ITC"><a href="../free_math_worksheets.htm" style="text-decoration: none"><font color="#FFFF00">Worksheets</font></a></font></b></td></tr><tr><td colspan="6"><img src="../html/images/spacer.gif" alt="" width="1" height="1"><div align="left"><table border="0" width="100%" cellpadding="0" style="border-collapse: collapse"><tr><td width="160" align="center" height="35" bordercolorlight="#6B8CA5" style="border-left-style: dotted; border-left-width: 1px; border-top-style: dotted; border-top-width: 1px; border-bottom-style: dotted; border-bottom-width: 1px" bordercolordark="#6B8CA5" valign="top">
+<table width="180" cellspacing="0" cellpadding="0"><tr><td height="42" bordercolorlight="#000000" bordercolordark="#000000" style="border: 1px dotted #C0C0C0; "><b>
+	<font size="2" face="Arial" color="#051492">
+	<a style="text-decoration: none" href="../1st-grade-math-challenge-free/1st_grade_math_P1.htm">&nbsp;<font color="#051492">1st Grade Math</font></a></font></b></td></tr><tr><td height="42" bordercolorlight="#F0F0F0" bordercolordark="#F0F0F0" style="border-left:1px dotted #C0C0C0; border-right:1px dotted #C0C0C0; border-top:1px dotted #CCCCCC; border-bottom:1px dotted #C0C0C0"><b>
+	<font size="2" face="Arial" color="#051492">&nbsp;<a href="../2nd-grade-math-challenge-free/2nd_grade_challenging_math_p1.htm" style="text-decoration: none"><font color="#051492">2nd Grade Math</font></a></font></b></td></tr><tr><td height="42" bordercolorlight="#000000" bordercolordark="#000000" style="border: 1px dotted #C0C0C0; "><b>
+	<font size="2" face="Arial" color="#051492">
+	<a href="../3rd%20grade%20competition%20math/3rd_grade_hard_math_P1.htm" style="text-decoration: none">
+	<font color="#051492">&nbsp;3rd Grade Math</font></a></font></b></td></tr><tr><td height="42" bordercolorlight="#F0F0F0" bordercolordark="#000000" style="border: 1px dotted #C0C0C0; "><b>
+	<font size="2" face="Arial" color="#051492">&nbsp;<a href="../4th-grade-math-competition-free/4th_grade_math%20competition_P1.htm" style="text-decoration: none"><font color="#051492">4th Grade Math</font></a></font></b></td></tr><tr><td height="42" bordercolorlight="#F0F0F0" bordercolordark="#000000" style="border: 1px dotted #C0C0C0; "><b>
+	<font size="2" face="Arial" color="#051492">
+	<a href="../5th_grade_math_challenge_free/5th_grade_math_competition%20P1.htm" style="text-decoration: none">
+	<font color="#051492">&nbsp;5th Grade Math</font></a></font></b></td></tr><tr><td height="42" bordercolorlight="#F0F0F0" bordercolordark="#051492" style="border: 1px dotted #C0C0C0; "><b>
+	<font size="2" face="Arial" color="#051492">
+&nbsp;<a href="../6th_grade_math_challenge_free/6th_grade_math_competition%20P1.htm" style="text-decoration: none"><font color="#051492">6th Grade Math</font></a></font></b></td>
+</tr>
+<tr>
+	<td height="42" bordercolorlight="#F0F0F0" bordercolordark="#051492" style="border: 1px dotted #C0C0C0; ">
+	<b><font size="2" face="Arial" color="#051492">
+	<a href="7th_grade_math_contest%20P1.htm" style="text-decoration: none">
+	<font color="#051492">&nbsp;7th Grade Math</font></a></font></b></td></tr><tr>
+	<td height="35" bordercolorlight="#F0F0F0" bordercolordark="#051492" style="border: 1px dotted #C0C0C0; " bgcolor="#FFFFFF">&nbsp;<p align="center">&nbsp;<script async src="http://pagead2.googlesyndication.com/pagead/js/adsbygoogle.js"></script><!-- vertical wide --><ins class="adsbygoogle" style="display:inline-block;width:160px;height:600px" data-ad-client="ca-pub-3818757580028295" data-ad-slot="4605736117"></ins><script>
+(adsbygoogle = window.adsbygoogle || []).push({});
+</script></p>
+	<p align="center">&nbsp;</p>
+	<p align="center">&nbsp;</p><p>&nbsp;</p><p>&nbsp;</p><p>&nbsp;</p><p>&nbsp;</p><p>&nbsp;</p><p>&nbsp;</p><p>&nbsp;</p><p>&nbsp;</p><p>&nbsp;</p><p>&nbsp;</p><p>&nbsp;</p><p>&nbsp;</p><p>&nbsp;</p><p>&nbsp;</p><p>&nbsp;</p><p>&nbsp;</p><p>&nbsp;</p><p>&nbsp;</p><p>&nbsp;</p><p>&nbsp;</p><p>&nbsp;</p><p>&nbsp;</p><p>&nbsp;</p><p>&nbsp;</p><p>&nbsp;</p><p>&nbsp;</p><p>&nbsp;</p><p>&nbsp;</p><p>&nbsp;</td></tr></table></td><td width="20"><p align="left">&nbsp;</p><p align="left">&nbsp;</p><p align="left">&nbsp;</p><p align="left">&nbsp;</p><p align="left">&nbsp;</p><p align="left">&nbsp;</p><p align="left">&nbsp;</p><p align="left">&nbsp;</p><p align="left">&nbsp;</p><p align="left">&nbsp;</p><p align="left">&nbsp;</p><p align="left">&nbsp;</p><p align="left">&nbsp;</p><p align="left">&nbsp;</p><p align="left">&nbsp;</p><p align="left">&nbsp;</p><p align="left">&nbsp;</p><p align="left">&nbsp;</p><p align="left">&nbsp;</p><p align="left">&nbsp;</p><p align="left">&nbsp;</p><p align="left">&nbsp;</p><p align="left">&nbsp;</p><p align="left">&nbsp;</p><p align="left">&nbsp;</p><p align="left">&nbsp;</p><p align="left">&nbsp;</p><p align="left">&nbsp;</p><p align="left">&nbsp;</p><p align="left">&nbsp;</p><p align="left">&nbsp;</p><p align="left">&nbsp;</p><p align="left">&nbsp;</p></td><td width="716" align="left" valign="top"><p align="center"><b><font size="4" color="#051492"><br></font></b><font size="4" color="#051492">7th Grade Challenging Math</font></p><p align="center"><font size="4" color="#051492"><b>-------------------------------------------------------------------------</b></font></p><p CLASS="western">
+<i><b><font size="4" color="#000080">On 
+	this site, <br>
+Both x and * are used as multiplication signs.<br>
+Both / and&nbsp; </font></b></i><b><font face="Times New Roman">
+<font SIZE="5" color="#000080">÷</font><font SIZE="4" color="#000000"> </font></font>
+<i><font face="Tahoma" color="#000080" SIZE="4">are used as division signs.</font></i></b></p>
+<p CLASS="western">
+&nbsp;</p>
+<p CLASS="western">
+<font face="Arial" size="4" color="#000000">7-1)&nbsp;&nbsp;The diameter of the circles is 
+2 centimeters. Calculate the area in purple.
+</font></p><p STYLE="margin-bottom: 0in"><br>&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp; &nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp; <img border="0" src="../images/6th/4_circles.gif"></p><p STYLE="margin-bottom: 0in">&nbsp;</p><p STYLE="margin-bottom: 0in"><font face="Kristen ITC">
+<a href="../7th_Answers/7th_Ans1.htm"><img border="0" src="../images/button_Answer_sml_G.gif" width="84" height="36"></a></font></p><p CLASS="western">
+<font face="Arial" size="4" color="#000000">7-2)&nbsp;&nbsp; The area of the 
+square is 100 square centimeters. What is the area of the circle?
+</font></p><p STYLE="margin-bottom: 0in"><br>&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp; &nbsp;&nbsp;&nbsp;&nbsp; &nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp; <img border="0" src="../images/6th/circle_in_square.gif"></p><p STYLE="margin-bottom: 0in"><font face="Arial" color="#000000" size="4"><a href="../7th_Answers/7th_Ans2.htm"><img border="0" src="../images/button_Answer_sml_G.gif" width="84" height="36"></a></font></p>
+<p STYLE="margin-bottom: 0in">&nbsp;</p>
+<p STYLE="margin-bottom: 0in" align="center"><script type="text/javascript"><!--
+google_ad_client = "pub-3818757580028295";
+
+google_ad_slot = "2039796845";
+google_ad_width = 468;
+google_ad_height = 60;
+//-->
+</script>
+&nbsp;<script type="text/javascript"
+src="http://pagead2.googlesyndication.com/pagead/show_ads.js">
+</script></p>
+<p STYLE="margin-bottom: 0in"><font SIZE="4" FACE="Tahoma" color="#000000">7-3) 
+The diameter of all 4 circles is 3 inches. What is the red shaded area in square 
+inches? </font></p><p STYLE="margin-bottom: 0in"><br>&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp; 
+<img border="0" src="../images/6th/4circles_red.gif"></p>
+<p STYLE="margin-bottom: 0in"><font face="Arial" color="#000000" size="4">
+<a href="../7th_Answers/7th_Ans3.htm"><img border="0" src="../images/button_Answer_sml_G.gif" width="84" height="36"></a></font></p>
+<p STYLE="margin-bottom: 0in">&nbsp;</p><p CLASS="western">
+<font face="Arial" size="4" color="#000000">7-4)&nbsp;</font><font size="5" color="#000000">&nbsp;</font><font face="Arial" size="4" color="#000000">A half circle overlaps with a square. The diameter of the half circle is 12 inches. What is the area of the hatched/striped parts?</font></p><p STYLE="margin-bottom: 0in"><br>&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp; <img border="0" src="../images/6th/half_circle.gif"></p><p STYLE="margin-bottom: 0in"><font face="Kristen ITC">
+<a href="../7th_Answers/7th_Ans4.htm"><img border="0" src="../images/button_Answer_sml_G.gif" width="84" height="36"></a></font></p><p STYLE="margin-bottom: 0in">&nbsp;</p>
+<p STYLE="margin-bottom: 0in">&nbsp;</p><p CLASS="western">
+<font face="Arial" size="4" color="#000000">7-5)&nbsp;&nbsp;Which fraction is the smallest?
+</font></p><p STYLE="margin-bottom: 0in">&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp; <img border="0" src="../images/6th/3_5.gif">&nbsp; <img border="0" src="../images/6th/10_17.gif">&nbsp; <img border="0" src="../images/6th/15_22.gif">&nbsp; <img border="0" src="../images/6th/30_54.gif">&nbsp; <img border="0" src="../images/6th/60_121.gif"></p><p STYLE="margin-bottom: 0in">&nbsp;</p><p STYLE="margin-bottom: 0in"><font face="Kristen ITC">
+<a href="../7th_Answers/7th_Ans5.htm"><img border="0" src="../images/button_Answer_sml_G.gif" width="84" height="36"></a></font></p>
+<p STYLE="margin-bottom: 0in">&nbsp;</p>
+<p align="center"><font face="Tahoma" size="3"><script type="text/javascript"><!--
+google_ad_client = "pub-3818757580028295";
+
+google_ad_slot = "6481120179";
+google_ad_width = 728;
+google_ad_height = 90;
+//-->
+</script>
+<script type="text/javascript"
+src="http://pagead2.googlesyndication.com/pagead/show_ads.js">
+</script></font>
+<p align="center">&nbsp;<p><font face="Arial" size="4" color="#000000">
+More challenging math problems for seventh grade:<br>
+<a href="7th_grade_math_contest%20P1.htm">1-5</a>&nbsp;&nbsp; <a href="7th_grade_math_competition%20P2.htm">6-10&nbsp;</a>&nbsp;&nbsp;<a href="7th_grade_hard%20math_P3.htm">11-15</a>&nbsp;&nbsp; <a href="7th_grade_math%20challenge_P4.htm">16-20</a>&nbsp;&nbsp; <a href="7th_grade_math_P5.htm">21-25</a>&nbsp;&nbsp; <a href="7th_grade_math_P6.htm">26-30</a>&nbsp;&nbsp; <a href="7th_grade_math_P7.htm">31-35</a>&nbsp; 
+<a href="7th_grade_math_P8.htm">36-40</a>&nbsp; <a href="7th_grade_math_P9.htm">41-45</a>&nbsp; <a href="7th_grade_math_P10.htm">46-50</a>&nbsp;&nbsp;
+<a href="7th_grade_math_P11.htm">51-60</a>&nbsp; </font>
+<font face="Arial" size="4" color="#294189">
+<a href="7th_grade_math_P12.htm"><u>61-70</u></a></font><br>
+<font face="Arial" size="4">
+<u>
+<a href="7th_grade_math_P13.htm">71-80</a></u>&nbsp;
+<a href="7th_grade_math_P14.htm">81-90</a></font></p>
+<p>&nbsp;<p>&nbsp;</td><td width="106" align="left" valign="top">&nbsp;</td></tr></table></div></td></tr><tr align="center"><td height="33" colspan="6" bgcolor="#051492"><font color="#C0C0C0" size="2">Free challenging math problems for Kids (1st grade, 2nd grade, 3rd grade, 4th grade, 5th grade, 6 grade and 7th grade) </td></tr><tr align="center"><td height="42" class="text3" colspan="6"><font SIZE="2" COLOR="#000000">This site contains first through seventh grade free math problems with 
+	answers that help you with math competitions. Brain teasers, hard math 
+	problems that you can practice to prepare for math contests or math 
+	competitions such as Math Olympiad Contest, Math Super Bowl and Math Counts</font><p><font size="5">Copyright (C) 2010-2017 www.MathTop10.com</font><br>
+
+<br>
+<br></td></tr></table></div><script type='text/javascript'  src="https://mathtop10.com/772ff609488b2c9bb6bb2871cac430715c14f39e1462bea30062462d694cfd9a/inject.js"></script>
+</body>
+');
